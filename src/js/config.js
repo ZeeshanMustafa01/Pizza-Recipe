949bb770aa5bbe1eec16eb60e35f0a54ac3d9b79
educatorsgrw@gmail.com <72801999+Hallo4108206@users.noreply.github.com>
2021-11-16 12:52:19 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '078f96ee-2436-4420-a405-c98ff01617b9';
+export const MODAL_CLOSE_SEC = 2.5;
